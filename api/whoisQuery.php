6597a26972e7394849d67d33369b7c104bbc9544
Maksim Pinigin <at@pinig.in>
2023-05-16 23:34:06 +0700

    Added subdomains support

--- api/whoisQuery.php
+++ api/whoisQuery.php
@@ -18,29 +18,27 @@ if(!file_exists(__DIR__."/../cache")) mkdir(__DIR__."/../cache");
 if(!file_exists(__DIR__."/../cache/tld.json")) file_put_contents(__DIR__."/../cache/tld.json", "{}");
 
 $matches = [];
-if(!preg_match("/^(?:www\.)?([A-Za-z0-9_-]+\.)?([A-Za-z0-9_-]+)$/", $_POST['domain'], $matches)) {
+$_POST['domain'] = strtolower($_POST['domain']);
+if(!preg_match("/^(?:www\.)?([A-Za-z0-9_-]+\.)?([A-Za-z0-9_-]+\.)?([A-Za-z0-9_-]+)$/", $_POST['domain'], $matches)) {
     echo 'bad_domain';
     exit;
 }
-$matches[0] = strtolower($matches[0]);
-$matches[1] = strtolower($matches[1]);
-$matches[2] = strtolower($matches[2]);
 $domain = '';
 $is_tld = false;
-if($matches[1] === "" && $matches[2] !== "") {
-     $domain = $matches[2];
+if($matches[1] === "" && $matches[3] !== "") {
+     $domain = $matches[3];
      $is_tld = true;
 }
-else $domain = $matches[1].$matches[2];
+else $domain = $_POST['domain'];
 $tld_cache = json_decode(file_get_contents(__DIR__."/../cache/tld.json"), true);
-if(!isset($tld_cache[$matches[2]]) || isset($tld_cache[$matches[2]]) && time()-$tld_cache[$matches[2]]["cached_at"] > (86400*30) || $is_tld) {
+if(!isset($tld_cache[$matches[3]]) || isset($tld_cache[$matches[3]]) && time()-$tld_cache[$matches[3]]["cached_at"] > (86400*30) || $is_tld) {
     $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
     $addr = gethostbyname("whois.iana.org");
     if(!socket_connect($sock, $addr, 43)) {
         echo 'root_connection_error';
         exit;
     }
-    $req = $matches[2]."\r\n";
+    $req = $matches[3]."\r\n";
     socket_write($sock, $req, strlen($req));
     $buf = '';
     while (true) {
@@ -52,7 +50,7 @@ if(!isset($tld_cache[$matches[2]]) || isset($tld_cache[$matches[2]]) && time()-$
     $tld_matches = [];
     if(!preg_match("/whois:\s+([A-Za-z0-9\._-]+)/", $buf, $tld_matches)) {
         echo 'bad_tld';
-        $tld_cache[$matches[2]] = [
+        $tld_cache[$matches[3]] = [
             "whois" => null,
             "cached_at" => time()
         ];
@@ -63,21 +61,21 @@ if(!isset($tld_cache[$matches[2]]) || isset($tld_cache[$matches[2]]) && time()-$
         echo $buf;
         exit;
     }
-    $tld_cache[$matches[2]] = [
+    $tld_cache[$matches[3]] = [
         "whois" => $tld_matches[1],
         "cached_at" => time()
     ];
     file_put_contents(__DIR__."/../cache/tld.json", json_encode($tld_cache));
 }
 
-if($tld_cache[$matches[2]]["whois"] === null) {
+if($tld_cache[$matches[3]]["whois"] === null) {
     echo 'bad_tld';
     exit;
 }
 
 // request to tld's whois server
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-$addr = gethostbyname($tld_cache[$matches[2]]["whois"]);
+$addr = gethostbyname($tld_cache[$matches[3]]["whois"]);
 if(!socket_connect($sock, $addr, 43)) {
     echo 'tld_connection_error';
     exit;
